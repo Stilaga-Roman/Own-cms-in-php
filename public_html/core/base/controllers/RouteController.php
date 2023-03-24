870947f71b16e059ce04d5d9348466afc35447a5
Roman Dudenko <predateljohn@gmail.com>
2023-03-25 01:48:11 +0400

    abc

---
+++ public_html/core/base/controllers/RouteController.php
@@ -0,0 +1,29 @@
+<?php
+namespace core\base\controllers;
+use core\base\settings\Settings;
+use core\base\settings\ShopSettings;
+class RouteController
+{
+    static private $_instance;
+
+    private function __clone()
+    {
+    }
+
+
+    static public function getInstance() {
+        if (self::$_instance instanceof self) {
+            return self::$_instance;
+        }
+        return self::$_instance = new self;
+    }
+
+    private function __construct()
+    {
+        $s = Settings::instance();
+        $s1 = ShopSettings::instance();
+        exit();
+    }
+
+
+}
